7e3755cfc83b27c0a1e337260ce52ebb3f630b08
William David Edwards <wedwards@cyberfusion.nl>
2025-09-01 20:15:37 +0200

    Update changelog, bump version

--- src/Client.php
+++ src/Client.php
@@ -20,7 +20,7 @@ class Client implements ClientContract
 
     private const TIMEOUT = 180;
 
-    private const VERSION = '1.119.0';
+    private const VERSION = '1.120.0';
 
     private const USER_AGENT = 'cyberfusion-cluster-api-client/' . self::VERSION;
 
